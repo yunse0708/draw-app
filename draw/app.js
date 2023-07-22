39e27923b5756aad057fec40faefd37ba087e9d7
yunse0708 <Yunse070708@gmail.com>
2023-07-22 21:48:06 +0900

    ADD :: 색상 변경

--- draw/app.js
+++ draw/app.js
@@ -1,3 +1,6 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -29,15 +32,22 @@ function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
-function onColorChange(event){
+function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  ctx.strokeStyle = event.target.dataset.color;
+  ctx.fillStyle = event.target.dataset.color;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting);
-
+canvas.addEventListener("mouseleave", cancelPainting)
+;
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
