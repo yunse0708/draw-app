412b744bc98cf04e5b00b0ef5b5b8a9dab387cb4
yunse0708 <Yunse070708@gmail.com>
2023-07-22 14:30:24 +0900

    ADD :: 색상 변경

--- draw/app.js
+++ draw/app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -13,20 +14,30 @@ function onMove(event) {
   }
   ctx.moveTo(event.offsetX, event.offsetY);
 }
+
 function startPainting() {
   isPainting = true;
 }
+
 function cancelPainting() {
   isPainting = false;
   ctx.beginPath();
 }
+
 function onLineWidthChange(event) {
   console.log(event.target.value);
   ctx.lineWidth = event.target.value;
 }
+
+function onColorChange(event){
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
