3f3dbecd1e4e9a6858455c7f91b57b914fbc7d65
yunse0708 <Yunse070708@gmail.com>
2023-07-22 23:56:00 +0900

    ADD :: 이미지 추가 기능

--- draw/app.js
+++ draw/app.js
@@ -1,3 +1,4 @@
+const fillInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
@@ -80,6 +81,17 @@ function onEraserClick(){
   modeBtn.innerText = "fill";
 }
 
+function onFileChange(event){
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function(){
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  };
+}
+
+canvas.onmousemove = onMove;
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -93,3 +105,4 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fillInput.addEventListener("change", onFileChange);
