cee7bfd9c54c2638e3469da872df7e113fb95bed
yunse0708 <Yunse070708@gmail.com>
2023-07-22 14:24:12 +0900

    ADD :: 선 굵기 조정

--- draw/app.js
+++ draw/app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 function onMove(event) {
   if (isPainting) {
@@ -17,9 +18,15 @@ function startPainting() {
 }
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
 }
-
+function onLineWidthChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
